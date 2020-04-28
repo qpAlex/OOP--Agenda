fa59955b87b3305f20008826c8a1df7c309ce48c
qpAlex <60066843+qpAlex@users.noreply.github.com>
2020-04-28 19:45:16 +0300

    First Commit
    Clasele finalizate - Acum urmeaza implementarea

---
+++ OOPagenda/OOPagenda/Activitate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPagenda
+{
+    class Activitate
+    {
+        //public List<Persoana> persoanaParticipa;
+        string nume;
+        string descriere;
+        DateTime dataInceput;
+        DateTime dataSfarsit;
+        public string Info_Activitate()
+        {
+            return $"{nume}-{descriere}.Inceput:{dataInceput.ToString()}. Sfarsit:{dataSfarsit.ToString()}.";
+        }
+    }
+}
---
+++ OOPagenda/OOPagenda/Data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPagenda
+{
+    class Data
+    {
+        int an;
+        int luna;
+        int zi;
+        string ora;
+        string minut;
+    }
+}
